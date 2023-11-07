7d78fe318059c7bc2f36c4c412d373c105beb0bb
clem.png <clem.netange@gmail.com>
2023-11-07 16:45:40 +0100

    correction Auth

--- src/classes/DataBase/ConnectionFactory.php
+++ src/classes/DataBase/ConnectionFactory.php
@@ -1,24 +1,25 @@
 <?php
 namespace touiteur\DataBase;
 use PDO ;
-class ConnectionFactory
-{
+class ConnectionFactory{
     public static ?PDO $db = null;
     private static array $config = [];
 
-    public static function setConfig($file)
-    {
+    public static function setConfig($file){
         self::$config = parse_ini_file($file);
     }
 
-    public static function makeConnection(array $config):PDO
-    {
-        self::$config = $config;
-        if (self::$db === null) {
+    public static function makeConnection(){
+        if (self::$db ==null) {
             $dsn = self::$config['driver'].
-            ':host='.self::$config['host'].
-            ';dbname='.self::$config['dbname'];
-            self::$db = new PDO($dsn, self::$config['user'], self::$config['password']);
+                ':host='.self::$config['host'].
+                ';dbname='.self::$config['database'];
+            self::$db = new PDO($dsn, self::$config['username'], self::$config['password'], [
+                PDO::ATTR_PERSISTENT=>true,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+
         }
         return self::$db;
     }
