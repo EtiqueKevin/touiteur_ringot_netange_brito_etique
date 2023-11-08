cbbcdd8d7ccb57d70d549820d0df624e71f188cb
4n0m4lie <mathias.ringot@gmail.com>
2023-11-08 15:02:09 +0100

    profil

--- src/classes/renderer/TouiteRenderer.php
+++ src/classes/renderer/TouiteRenderer.php
@@ -20,8 +20,11 @@ class TouiteRenderer{
                 try {
                     $html .= '<div id="touite">
                 <h2 class="touite-author"><a href="utilisateur.php">'.$this->touite->auteur.'</a> </h2><p>'.$this->touite->aff_date().'</p><br></div> 
-                <p class="touite-content">'.$this->touite->texte.'</p><br>
-                ';
+                <p class="touite-content">'.$this->touite->texte.'</p><br>';
+                $html .= $this->touite->photo != null ? "<image>" : "";
+                $html .='</div> <a href="../index.html"><button id="like">like</button></a></p>';
+
+
                 } catch (InvalidPropertyValueException $e) {
                     echo $e->getMessage();
                 }
