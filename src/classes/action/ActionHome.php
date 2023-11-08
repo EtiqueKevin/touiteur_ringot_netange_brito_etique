2a37dd79e63788fa51cdf632d640024895908425
etique <etique5u@etu.univ-lorraine.fr>
2023-11-08 11:23:58 +0100

    correction home 2

--- src/classes/action/ActionHome.php
+++ src/classes/action/ActionHome.php
@@ -1,12 +1,15 @@
 <?php
 
 namespace touiteur\action;
+use touiteur\Home\Home;
 
+require_once 'vendor/autoload.php';
 class ActionHome{
     public function execute(): string{
         $html = <<<HTML
         <h1>Home page</h1>
         HTML;
+        $html .= Home::afficherTouit();
         return $html;
     }
 
