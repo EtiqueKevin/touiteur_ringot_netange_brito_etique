b27420c9b808fc0646f5582e65a67d6b0a1af557
4n0m4lie <mathias.ringot@gmail.com>
2023-11-08 11:13:09 +0100

    profil

--- src/classes/action/ActionProfile.php
+++ src/classes/action/ActionProfile.php
@@ -12,7 +12,7 @@ class ActionProfile extends Action{
 
     public function execute(): string{
 
-        $html = (new UtilisateurRenderer($_SESSION['user']))->render(1);
+        $html = (new UtilisateurRenderer(unserialize($_SESSION['user'])))->render(1);
         return $html;
     }
 
