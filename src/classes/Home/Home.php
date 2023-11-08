7dc028106196ec01413eeb95a6dc31bcd699fb76
etique <etique5u@etu.univ-lorraine.fr>
2023-11-08 10:54:07 +0100

    home.php

---
+++ src/classes/Home/Home.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace touiteur\Home;
+
+use touiteur\DataBase\ConnectionFactory;
+
+class Home{
+    public static function afficherTouit(): string{
+        $db = ConnectionFactory::makeConnection();
+        $statement = $db->prepare("Select * from Touite");
+        $res = $statement->execute();
+        $touites = $statement->fetchAll();
+        $listeTouite = new ListeTouite();
+        foreach ($touites as $touite) {
+            $nouveauTouite = new Touite($touite['id'], $touite['text'], $touite['date'], $touite['author']);
+            $listeTouite->addTouite($nouveauTouite);
+        }
+        $listeTouiteRenderer = new ListeTouitesRenderer($listeTouite);
+        return $listeTouiteRenderer->render(1);
+    }
+}
