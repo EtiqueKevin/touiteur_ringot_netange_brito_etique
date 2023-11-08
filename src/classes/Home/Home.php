1896e319083cb4bb3993ad681dc34c17234cffb8
clem-png <112936771+clem-png@users.noreply.github.com>
2023-11-08 15:34:50 +0100

    Update Home.php

--- src/classes/Home/Home.php
+++ src/classes/Home/Home.php
@@ -15,11 +15,51 @@ class Home{
         $touites = $statement->fetchAll();
         $listeTouite = new ListeTouite();
         foreach ($touites as $touite) {
-            $img = $touite['img'] != null ? $touite['img'] : null;
-            $nouveauTouite = new Touite($touite['id'], $touite['text'], $touite['date'], $touite['pseudo'], $img);
-            $listeTouite->addTouite($nouveauTouite);
+            $nouveauTouite = null;
+            if($touite['img'] == null){
+                $nouveauTouite = new Touite($touite['id'], $touite['text'], $touite['date'], $touite['pseudo']);
+                $listeTouite->addTouite($nouveauTouite);}
+            else{
+            $nouveauTouite = new Touite($touite['id'], $touite['text'], $touite['date'], $touite['pseudo'], $touite['img']);
+            $listeTouite->addTouite($nouveauTouite);}
         }
         $listeTouiteRenderer = new ListeTouitesRenderer($listeTouite);
-        return $listeTouiteRenderer->render();
+        return $listeTouiteRenderer->render(1);
+    }
+
+    public static function afficherTouitesSuivie(): string{
+        $db = ConnectionFactory::makeConnection();
+
+        $st1= $db->prepare("Select followed from FollowedUser where follower = ?");
+        $st1->bindParam(1, $_SESSION['user']->email);
+        $st1->execute();
+        $row1 = $st1->fetch();
+        $emailRes = implode(',', $row1);
+
+        $st2= $db->prepare("Select id from FollowTag inner join TouiteTag on FollowTag.tag = TouiteTag.tag where FollowTag.email = ?");
+        $st2->bindParam(1, $_SESSION['user']->email);
+        $st2->execute();
+        $row2 = $st2->fetch();
+        $idRes = implode(',', $row2);
+
+        $statement = $db->prepare("Select * from Touite where id IN ? OR author IN ? order by date desc");
+        $statement->bindParam(1, $idRes);
+        $statement->bindParam(2, $emailRes);
+        $statement->execute();
+        $touites = $statement->fetchAll();
+
+        $listeTouite = new ListeTouite();
+
+        foreach ($touites as $touite) {
+            $nouveauTouite = null;
+            if($touite['img'] == null){
+                $nouveauTouite = new Touite($touite['id'], $touite['text'], $touite['date'], $touite['pseudo']);
+                $listeTouite->addTouite($nouveauTouite);}
+            else{
+                $nouveauTouite = new Touite($touite['id'], $touite['text'], $touite['date'], $touite['pseudo'], $touite['img']);
+                $listeTouite->addTouite($nouveauTouite);}
+        }
+        $listeTouiteRenderer = new ListeTouitesRenderer($listeTouite);
+        return $listeTouiteRenderer->render(1);
     }
 }
