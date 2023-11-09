5ab01113a956b521d05bc8340b4063ad3146a2b8
etique <etique5u@etu.univ-lorraine.fr>
2023-11-09 10:40:48 +0100

    action like 3

--- src/classes/action/ActionLike.php
+++ src/classes/action/ActionLike.php
@@ -10,21 +10,51 @@ class ActionLike extends Action
     {
         $html = '';
         if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-            $id = $_GET['id'];
-            $bd = ConnectionFactory::makeConnection();
-            $sql = "UPDATE touites SET likes = likes + 1 WHERE id = ?";
-            $st = $bd->prepare($sql);
-            $st->bindParam(1, $id);
-            $st->execute();
-            if ($_SESSION['position'] == 'home') {
-                $ah = new ActionHome();
-                $html = $ah->execute();
-            } else if ($_SESSION['position'] == 'profile') {
-                $ap = new ActionProfile();
-                $html = $ap->execute();
-            } else if ($_SESSION['position'] == 'discover') {
-                $ad = new ActionDiscover();
-                $html = $ad->execute();
+            $user = unserialize($_SESSION['user']);
+            $user->email;
+            if (!isset($_SESSION['user'])) {
+                $id = $_GET['id'];
+                $bd = ConnectionFactory::makeConnection();
+                $sql = "SELECT * FROM HasLiked where email = ? and idTouite = ?";
+                $st = $bd->prepare($sql);
+                $st->bindParam(1, $user->email);
+                $st->bindParam(2, $id);
+                $st->execute();
+                $result = $st->fetch();
+                if (!$result) {
+                    $sql = "INSERT INTO HasLiked (email, idTouite) VALUES (?, ?)";
+                    $st = $bd->prepare($sql);
+                    $st->bindParam(1, $user->email);
+                    $st->bindParam(2, $id);
+                    $st->execute();
+                    $sql = "UPDATE touites SET likes = likes + 1 WHERE id = ?";
+
+                } else {
+                    $sql = "DELETE FROM HasLiked WHERE email = ? and idTouite = ?";
+                    $st = $bd->prepare($sql);
+                    $st->bindParam(1, $user->email);
+                    $st->bindParam(2, $id);
+                    $st->execute();
+                    $sql = "UPDATE touites SET likes = likes - 1 WHERE id = ?";
+                }
+                $st = $bd->prepare($sql);
+                $st->bindParam(1, $id);
+                $st->execute();
+                if ($_SESSION['position'] == 'home') {
+                    $ah = new ActionHome();
+                    $html = $ah->execute();
+                } else if ($_SESSION['position'] == 'profile') {
+                    $ap = new ActionProfile();
+                    $html = $ap->execute();
+                } else if ($_SESSION['position'] == 'discover') {
+                    $ad = new ActionDiscover();
+                    $html = $ad->execute();
+                }
+            }
+
+            else{
+                $ac = new ActionConnexion();
+                $html = $ac->execute();
             }
         }
         return $html;
