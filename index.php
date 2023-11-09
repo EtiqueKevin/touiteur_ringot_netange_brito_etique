f443d3fc936b3e52606a932e8925393eb97b1796
ETIQUE Kevin <ketique04@gmail.com>
2023-11-09 08:17:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ use touiteur\dispatch\Dispatcher;
 use touiteur\DataBase\ConnectionFactory;
 
 require_once 'vendor/autoload.php';
-require_once 'src/classes/action/lib_touiteur.php';
 
 ConnectionFactory::setConfig("conf/config.ini");
 
