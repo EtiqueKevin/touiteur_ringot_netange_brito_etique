8ef36874ab4c2c7403e0d0493385a32f9b5316b1
4n0m4lie <mathias.ringot@gmail.com>
2023-11-10 12:31:15 +0100

    profil

--- src/classes/renderer/TouiteRenderer.php
+++ src/classes/renderer/TouiteRenderer.php
@@ -29,10 +29,10 @@ class TouiteRenderer{
                     $html .= '<div id="block-touite">
                 <div id="touite">
                 <div id="touite-head">
-                <h2 class="touite-author"><a href="?action=ActionUtilisateur&pseudo='.$this->touite->auteur.'">'.$this->touite->auteur.'</a></h2><br></div>
-                <p class="touite-content">'.$text.'</p><br>';
+                <h2 class="touite-author"><a href="?action=ActionUtilisateur&pseudo='.$this->touite->auteur.'&page=1">'.$this->touite->auteur.'</a></h2><br></div>
+                <a href="?action=display-touite&idTouite='.$this->touite->id.'"><p class="touite-content">'.$text.'</p><br>';
                 $html .= $this->touite->photo != null ? "image..." : "";
-                $html .= Utilisateur::hasLiked($user->email, $this->touite->id) ? '</div> <a href="?action=like&id='.$this->touite->id.'"><img id="unlike" src="ressources/Heart.png"></a></p></div>' : '</div> <a href="?action=like&id='.$this->touite->id.'"><img id="like" src="ressources/Heart.png"></a></p></div>';
+                $html .= Utilisateur::hasLiked($user->email, $this->touite->id) ? '</a></div> <a href="?action=like&id='.$this->touite->id.'"><img id="unlike" src="ressources/Heart.png"></a></p></div>' : '</a></div> <a href="?action=like&id='.$this->touite->id.'"><img id="like" src="ressources/Heart.png"></a></p></div>';
 
 
                 } catch (InvalidPropertyValueException $e) {
@@ -46,9 +46,9 @@ class TouiteRenderer{
                 <div id="touite">
                 <div id="touite-head">
                 <h2 class="touite-author"><a href="?action=ActionUtilisateur&pseudo='.$this->touite->auteur.'">'.$this->touite->auteur.'</a></h2><br></div>
-                <p class="touite-content">'.$text.'</p><br>';
+                <a href="?action=display-touite&idTouite='.$this->touite->id.'"><p class="touite-content">'.$text.'</p><br>';
                 $html .= $this->touite->photo != null ? "image..." : "";
-                $html .= '</div></div>';
+                $html .= '</a></div></div>';
                 } catch (InvalidPropertyValueException $e) {
                     echo $e->getMessage();
                 }
@@ -61,8 +61,8 @@ class TouiteRenderer{
                 <div id="touite">
                 <div id="touite-head">
                 <h2 class="touite-author">'.$this->touite->auteur.'</h2><h2>'. $this->touite->aff_date(). '</h2><br></div>
-                <p class="touite-content-full">'.$text.'</p><br>';
-                $html .= $this->touite->photo != null ? '<img src="'.$this->touite->photo.'">' : "";
+                <a href="?action=display-touite&idTouite='.$this->touite->id.'"><p class="touite-content-full">'.$text.'</p><br>';
+                $html .= $this->touite->photo != null ? '<img src="'.$this->touite->photo.'"></a>' : "";
                 $html .= '</div> <div id="likes-counter"> <p class="number-likes">'. Touite::getLikes($this->touite->id) . ' Likes</p>';
                 $html .= Utilisateur::hasLiked($user->email, $this->touite->id) ? '<a href="?action=like&id='.$this->touite->id.'"><img id="unlike" src="ressources/Heart.png"></a></p></div></div>' : '<a href="?action=like&id='.$this->touite->id.'"><img id="like" src="ressources/Heart.png"></a></p></div></div>';
 
@@ -79,9 +79,9 @@ class TouiteRenderer{
                 <div id="touite">
                 <div id="touite-head">
                 <h2 class="touite-author">'.$this->touite->auteur.'</h2><h2>'. $this->touite->aff_date(). '</h2><br></div>
-                <p class="touite-content-full">'.$text.'</p><br>';
+                <a href="?action=display-touite&idTouite='.$this->touite->id.'"><p class="touite-content-full">'.$text.'</p><br>';
                 $html .= $this->touite->photo != null ? '<img src="'.$this->touite->photo.'">' : "";
-                $html .= '</div> <div id="likes-counter"> <p class="number-likes">'. Touite::getLikes($this->touite->id) . ' Likes</p>';
+                $html .= '</a></div> <div id="likes-counter"> <p class="number-likes">'. Touite::getLikes($this->touite->id) . ' Likes</p>';
                 $html .= '</div></div>';
 
 
